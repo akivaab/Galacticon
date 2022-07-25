58beaf1651b418b41e9691379494fb0d14e7a3c8
akivaab <amabramo18@gmail.com>
2022-07-25 21:40:21 +0300

    basic movement mechanics

--- main.py
+++ main.py
@@ -15,10 +15,21 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("assets/player.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
+
+# init enemy
+enemyImg = pygame.image.load("assets/enemy1.png")
+enemyX = 370
+enemyY = 100
+enemyX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # game loop
@@ -31,5 +42,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # arrow keystrokes
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX < 0:
+        playerX = 0
+    elif playerX > 736:
+        playerX = 736
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
