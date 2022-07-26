661d5f0d32fc27d6bb5cbc17c28b5c4bd8b3817a
akivaab <amabramo18@gmail.com>
2022-07-26 21:26:56 +0300

    fire one bullet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize
 pygame.init()
@@ -19,9 +20,18 @@ playerX_change = 0
 
 # init enemy
 enemyImg = pygame.image.load("assets/enemy1.png")
-enemyX = 370
-enemyY = 100
-enemyX_change = 0
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.2
+enemyY_change = 40
+
+# init bullet
+bulletImg = pygame.image.load("assets/bullet.png")
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 0.5
+bulletState = "ready"
 
 
 def player(x, y):
@@ -30,6 +40,12 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bulletState
+    bulletState = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 # game loop
@@ -48,6 +64,8 @@ while running:
                 playerX_change = -0.2
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.2
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -57,6 +75,20 @@ while running:
         playerX = 0
     elif playerX > 736:
         playerX = 736
+
+    enemyX += enemyX_change
+    if enemyX < 0:
+        enemyX_change = 0.2
+        enemyY += enemyY_change
+    elif enemyX > 736:
+        enemyX_change = -0.2
+        enemyY += enemyY_change
+
+    # bullet movement
+    if bulletState == "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
