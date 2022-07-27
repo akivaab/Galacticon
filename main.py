3a446ef2fde0fa189fba5acadb33fd60dfcc3a5e
akivaab <amabramo18@gmail.com>
2022-07-27 21:26:41 +0300

    enemies, bullets, and a score to settle

--- main.py
+++ main.py
@@ -9,37 +9,55 @@ screen = pygame.display.set_mode((800, 600))
 
 # screen window caption and icon
 pygame.display.set_caption("Galacticon")
-icon = pygame.image.load("assets/logo.png")
+icon = pygame.image.load("assets/logo.png").convert()
 pygame.display.set_icon(icon)
-
+print(pygame.font.get_fonts())
 # init player
-playerImg = pygame.image.load("assets/player.png")
+playerImg = pygame.image.load("assets/player.png").convert()
 playerX = 370
 playerY = 480
 playerX_change = 0
 
 # init enemy
-enemyImg = pygame.image.load("assets/enemy1.png")
-enemyX = random.randint(0, 736)
-enemyY = random.randint(50, 150)
-enemyX_change = 0.2
-enemyY_change = 40
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_enemies = 6
+
+for i in range(num_enemies):
+    enemyImg.append(pygame.image.load("assets/enemy1.png").convert())
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(0.2)
+    enemyY_change.append(40)
 
 # init bullet
-bulletImg = pygame.image.load("assets/bullet.png")
+bulletImg = pygame.image.load("assets/bullet.png").convert()
 bulletX = 0
 bulletY = 480
 bulletX_change = 0
 bulletY_change = 0.5
 bulletState = "ready"
 
+score_value = 0
+font = pygame.font.Font('assets/PressStart2P-vaV7.ttf', 16)
+textX = 10
+textY = 10
+
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, enemy_num):
+    screen.blit(enemyImg[enemy_num], (x, y))
 
 
 def fire_bullet(x, y):
@@ -64,8 +82,9 @@ while running:
                 playerX_change = -0.2
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.2
-            if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)
+            if event.key == pygame.K_SPACE and bulletState == "ready":
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -76,19 +95,36 @@ while running:
     elif playerX > 736:
         playerX = 736
 
-    enemyX += enemyX_change
-    if enemyX < 0:
-        enemyX_change = 0.2
-        enemyY += enemyY_change
-    elif enemyX > 736:
-        enemyX_change = -0.2
-        enemyY += enemyY_change
+    for i in range(num_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] < 0:
+            enemyX_change[i] = 0.2
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] > 736:
+            enemyX_change[i] = -0.2
+            enemyY[i] += enemyY_change[i]
+
+        # collision
+        enemyRect = pygame.rect.Rect(enemyX[i] + 8, enemyY[i] + 8, 48, 48)
+        bulletRect = pygame.rect.Rect(bulletX + 28, bulletY, 8, 32)
+        collision = enemyRect.colliderect(bulletRect)
+        if collision:
+            bulletY = 480
+            bulletState = "ready"
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+            score_value += 1
+
+        enemy(enemyX[i], enemyY[i], i)
 
     # bullet movement
+    if bulletY < 0:
+        bulletY = 480
+        bulletState = "ready"
     if bulletState == "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+    show_score(textX, textY)
     pygame.display.update()
